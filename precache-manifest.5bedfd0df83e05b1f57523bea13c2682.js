5d04b9a9112b4a87514692df0e083ebd7c508101
Abhishekgowda KM <34267@ITCINFOTECH.COM>
2020-06-17 13:43:33 +0530

    Updates

---
+++ precache-manifest.5bedfd0df83e05b1f57523bea13c2682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d56c9593c2291f468563e980727b7c7",
+    "url": "/Movie-search-with-react-and-redux/index.html"
+  },
+  {
+    "revision": "e8e318a89dcaa55946c9",
+    "url": "/Movie-search-with-react-and-redux/static/css/main.6b278a33.chunk.css"
+  },
+  {
+    "revision": "810cc31be53d1a7c9945",
+    "url": "/Movie-search-with-react-and-redux/static/js/2.01d9c7da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie-search-with-react-and-redux/static/js/2.01d9c7da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e318a89dcaa55946c9",
+    "url": "/Movie-search-with-react-and-redux/static/js/main.995ad676.chunk.js"
+  },
+  {
+    "revision": "21d47dbede21ab99cfe7",
+    "url": "/Movie-search-with-react-and-redux/static/js/runtime-main.7dd83de8.js"
+  }
+]);
